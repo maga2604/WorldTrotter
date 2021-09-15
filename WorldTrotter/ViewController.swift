b7c3ac3239aba6a129a5d07f064199891ab486ef
Maga <maga.rajic2604@gmail.com>
2021-09-15 00:54:33 -0700

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Maga Rajic on 15.09.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
