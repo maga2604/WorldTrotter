0c3a1c416947e97cac0eee4cc1894ec9130d5f20
Maga <maga.rajic2604@gmail.com>
2021-09-15 12:22:15 -0700

    View Controller

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Maga Rajic on 15.09.2021..
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+    
+}
